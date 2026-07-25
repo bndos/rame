8eff1ea0c4a6bf08e7b6aa632f65c2234bd17691
Brando <grover-brando.tovar-oblitas@polymtl.ca>
2026-07-25 00:06:03 -0400

    Add more config to ort

--- src/session/ort/config.rs
+++ src/session/ort/config.rs
@@ -1,3 +1,4 @@
+use ort::ep;
 use ort::session::builder::SessionBuilder;
 
 use crate::session::ort::OrtError;
@@ -7,6 +8,7 @@ use crate::session::ort::OrtError;
 pub struct OrtSessionConfig {
     pub(super) intra_threads: Option<usize>,
     pub(super) inter_threads: Option<usize>,
+    pub(super) execution_provider: OrtExecutionProvider,
 }
 
 impl OrtSessionConfig {
@@ -20,7 +22,53 @@ impl OrtSessionConfig {
         self
     }
 
+    pub fn cuda(mut self, device_id: i32) -> Self {
+        self.execution_provider = OrtExecutionProvider::Cuda { device_id };
+        self
+    }
+
+    pub fn tensorrt(mut self, device_id: i32) -> Self {
+        self.execution_provider = OrtExecutionProvider::TensorRt {
+            device_id,
+            fp16: false,
+        };
+        self
+    }
+
+    pub fn tensorrt_fp16(mut self, device_id: i32) -> Self {
+        self.execution_provider = OrtExecutionProvider::TensorRt {
+            device_id,
+            fp16: true,
+        };
+        self
+    }
+
     pub(super) fn apply(self, mut builder: SessionBuilder) -> Result<SessionBuilder, OrtError> {
+        builder = match self.execution_provider {
+            OrtExecutionProvider::Cpu => builder,
+            OrtExecutionProvider::Cuda { device_id } => builder
+                .with_execution_providers([
+                    ep::CUDA::default()
+                        .with_device_id(device_id)
+                        .build()
+                        .error_on_failure(),
+                ])
+                .map_err(OrtError::from)?,
+            OrtExecutionProvider::TensorRt { device_id, fp16 } => builder
+                .with_execution_providers([
+                    ep::TensorRT::default()
+                        .with_device_id(device_id)
+                        .with_fp16(fp16)
+                        .build()
+                        .error_on_failure(),
+                    ep::CUDA::default()
+                        .with_device_id(device_id)
+                        .build()
+                        .error_on_failure(),
+                ])
+                .map_err(OrtError::from)?,
+        };
+
         if let Some(threads) = self.intra_threads {
             builder = builder
                 .with_intra_threads(threads)
@@ -35,4 +83,12 @@ impl OrtSessionConfig {
 
         Ok(builder)
     }
+}
+
+#[derive(Debug, Clone, Copy, Default)]
+pub(super) enum OrtExecutionProvider {
+    #[default]
+    Cpu,
+    Cuda { device_id: i32 },
+    TensorRt { device_id: i32, fp16: bool },
 }
