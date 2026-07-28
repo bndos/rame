c7c2cb14164c0f647a0ae9afd88bbf48df709b62
Brando <grover-brando.tovar-oblitas@polymtl.ca>
2026-07-28 19:04:15 -0400

    Move datasets to registry

--- benchmarks/src/rame_benchmarks/datasets/__init__.py
+++ benchmarks/src/rame_benchmarks/datasets/__init__.py
@@ -1,25 +1,13 @@
 from __future__ import annotations
 
-from dataclasses import dataclass
-from enum import Enum
+from rame_benchmarks.datasets.registry import (
+    Dataset,
+    DatasetName,
+    supported_dataset,
+)
 
-
-class DatasetName(str, Enum):
-    DOCLAYOUT_PUBLAYNET = "doclayout-publaynet"
-
-
-@dataclass(frozen=True)
-class Dataset:
-    name: DatasetName
-    repo_id: str
-    split: str
-
-
-def supported_dataset(name: DatasetName) -> Dataset:
-    match name:
-        case DatasetName.DOCLAYOUT_PUBLAYNET:
-            return Dataset(
-                name=name,
-                repo_id="creative-graphic-design/PubLayNet",
-                split="test",
-            )
+__all__ = [
+    "Dataset",
+    "DatasetName",
+    "supported_dataset",
+]
